8aa31e7a1db549fb891e67f57d648d3b8a73ecd2
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-08-27 16:39:31 -0300

    versao 0.3, fazendo o card e melhorando o codigo

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,12 @@
 let card = document.querySelector('.card');
 
-async function handleDados(url) {
+async function load(url) {
   let response = await fetch(url);
-  let responseJson = await response.json();
-  return responseJson;
+  let data = await response.json();
+  return data;
 }
 
-let dados = handleDados('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+let dados = load('https://pokeapi.co/api/v2/pokemon?limit=200&offset=0');
 
 dados.then((data) => {
   for (let i in data.results) {
@@ -20,11 +20,44 @@ function filterName(name) {
   return newName;
 }
 
-function template(dadosPokemons) {
-  card.innerHTML += `<h3>${filterName(dadosPokemons.name)}</h3>`
-  card.innerHTML += `<small>${async function () {
-    let url = await handleDados(dadosPokemons.url);
-  }()
-    }
-  </small>`
+async function template(dadosPokemons) {
+  let dadosUrl = await load(dadosPokemons.url);
+  card.innerHTML +=
+   `<div class="card1">
+      <img src="${dadosUrl.sprites.front_default}"></img>
+       <div class="container">
+          <h3>${filterName(dadosPokemons.name)}</h3>
+          <p> tipo: ${dadosUrl.types.map(type => type.type.name)}</p>
+      </div>
+   </div>`
 }
+
+// const dadosImage = {
+//   isFront: true,
+//   currentImg: ''
+// }
+
+// let card1 = document.querySelector('.card1');
+// card1.addEventListener('click', function(){
+//   mudarSprite(dadosPokemons);
+// })
+// function mudarSprite(dadosPokemons){
+//   if(dadosImage.isFront){
+//     dadosImage.isFront = false;
+//     dadosImage.currentImg = dadosPokemons.sprites.back_default;
+//   }else {
+//     dadosImage.isFront = true;
+//     dadosImage.currentImg = dadosPokemons.sprites.front_default;
+//   }
+// }
+
+
+// ${async function () {
+//   let url = await handleDados(dadosPokemons.url);
+//   let dadosUrl = new Object();
+//   dadosUrl.urlTypes = url.types;
+//   dadosUrl.backDefault = url.sprites.back_default;
+//   dadosUrl.FrontDefault = url.sprites.front_default;
+//   console.log(dadosUrl.FrontDefault)
+// }()
+//   }
