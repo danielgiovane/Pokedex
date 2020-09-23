f1f6f5c46b711f2b40ee5277cbe2c601614a081c
Daniel Fernandes <danielgiovane2015@gmail.com>
2020-09-23 12:16:03 -0300

    Versão 0.8, transformando funções em funções de setas

--- public/js/index.js
+++ public/js/index.js
@@ -2,40 +2,32 @@ let card = document.querySelector('.card');
 
 const getUrlPokemon = (id) => ` https://pokeapi.co/api/v2/pokemon/${id} `;
 
+const handleDataOfPokemons = (res) => res.ok ? res.json() : Promise.reject(statusText);
+
+const insertPokemonPage = (pokemon) => card.innerHTML = template(pokemon)
+
 const generatePokemonPromises = () => Array(201).fill().map((item, index) =>
-  fetch(getUrlPokemon(index + 1)).then(handleDataPokemon)
+  fetch(getUrlPokemon(index + 1)).then(handleDataOfPokemons)
 );
 
-const fetchPokemon = () => {
-  const arrPokemons = generatePokemonPromises();
-  Promise.all(arrPokemons)
-    .then(pokemons => {
-      card.innerHTML = template(pokemons)
-    })
-};
-
-fetchPokemon();
+const arrPokemons = generatePokemonPromises();
+Promise.all(arrPokemons)
+  .then(insertPokemonPage)
 
 
-function handleDataPokemon(res) {
-  return res.ok ? res.json() : Promise.reject(statusText);
-};
-
-function filterName(name) {
+const filterName = (name) => {
   let newName = name[0].toUpperCase() + name.slice(1);
   return newName;
 };
 
-const template = (pokemons) => {
-  return pokemons.reduce((acc, pokemon) => {
-    const elTypes = pokemon.types.map(typeInfo => typeInfo.type.name)
-    return acc +=
-      ` <div class="card1 ${elTypes[0]}">
-          <img class="card-image " alt="${pokemon.name}"src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+const template = (pokemons) => pokemons.reduce((acc, { name, types, id }) => {
+  const elTypes = types.map(typeInfo => typeInfo.type.name)
+  return acc +=
+    ` <div class="card1 ${elTypes[0]}">
+          <img class="card-image " alt="${name}"src="https://pokeres.bastionbot.org/images/pokemon/${id}.png"/>
           <div class="container">
-            <h3 class="nome">${filterName(pokemon.name)}</h3>
+            <h3 class="nome">${filterName(name)}</h3>
             <p class="info">Tipo: ${elTypes.join(' | ')}</p>
           </div>
         </div>`
-  }, '');
-};
+}, '');
